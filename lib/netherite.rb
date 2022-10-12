4c695d4f61ebaa447833cb519d8f94096d778174
inrage <70843287+inrag3@users.noreply.github.com>
2022-10-12 23:33:51 +0300

    initial commit

---
+++ lib/netherite.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "netherite/version"
+
+module Netherite
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/netherite/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Netherite
+  VERSION = "0.1.0"
+end
