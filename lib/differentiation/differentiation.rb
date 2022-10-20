1b52706ac9d4c532af5a3e4554fc3bc67633668e
learde <leardeleo@gmail.com>
2022-10-20 21:46:32 +0300

    now function diff can parse ax+b (a,b>0)

--- lib/differentiation/differentiation.rb
+++ lib/differentiation/differentiation.rb
@@ -12,42 +12,75 @@ module Differentiation
       pp ast
 
       @var = var
+      @indent = 0
 
+      ast.print
+      puts "\n\n\n"
       recurDiff(ast)
-      pp ast
+      ast.print
     end
 
     private
     def recurDiff(root)
-      pp root.value
-      case root
-      when Netherite::BinOpNode
+      case root.nodeType
+      when Netherite::NodeType::BINNODE
         handleBinOp(root)
       end
     end
 
     def handleBinOp(node)
+      unless searchDiffVar(node)
+        node.replace_this_node(Netherite::Token.new(Netherite::TokenType::NUMBER, 0))
+      end
       case node.token.type
       when Netherite::TokenType::PLUS
         handleNodeAfterPlus(node.left)
         handleNodeAfterPlus(node.right)
+        tryToDeletePlus(node)
+      when Netherite::TokenType::MULTIPLY
+        handleNodeAfterMultiply(node.left)
+        handleNodeAfterMultiply(node.right)
+        tryToDeleteMult(node)
       end
     end
 
     def handleNodeAfterPlus(node)
-      case node
-      when Netherite::NumberNode
+      case node.nodeType
+      when Netherite::NodeType::NUMBER
         node.value = 0
-      when Netherite::VarNode
-        if node.value != @var
-          if node.parent.left == node
-            node.parent.left = Netherite::NumberNode.new(Netherite::Token.new(Netherite::TokenType::NUMBER, 0))
-            node.parent.left.parent = node.parent
-          elsif node.parent.right == node
-            node.parent.right = Netherite::NumberNode.new(Netherite::Token.new(Netherite::TokenType::NUMBER, 0))
-            node.parent.right.parent = node.parent
-          end
+      when Netherite::NodeType::VAR
+        node.replace_this_node(Netherite::Token.new(Netherite::TokenType::NUMBER, node.value != @var ? 0 : 1))
+      else
+        recurDiff(node)
+      end
+    end
+
+    def handleNodeAfterMultiply(node)
+      case node.nodeType
+      when Netherite::NodeType::VAR
+        if node.value == @var
+          node.replace_this_node(Netherite::Token.new(Netherite::TokenType::NUMBER, 1))
         end
+      end
+    end
+
+    def searchDiffVar(node)
+      return false if node.nil?
+      return true if node.nodeType == Netherite::NodeType::VAR && node.value == @var
+
+      return searchDiffVar(node.left) || searchDiffVar(node.right)
+    end
+
+    def tryToDeleteMult(node)
+      if node.left.nodeType == Netherite::NodeType::NUMBER && node.right.nodeType == Netherite::NodeType::NUMBER
+        new_value = node.left.value.to_i * node.right.value.to_i
+        node.replace_this_node(Netherite::Token.new(Netherite::TokenType::NUMBER, new_value))
+      end
+    end
+    def tryToDeletePlus(node)
+      if node.left.nodeType == Netherite::NodeType::NUMBER && node.right.nodeType == Netherite::NodeType::NUMBER
+        new_value = node.left.value.to_i + node.right.value.to_i
+        node.replace_this_node(Netherite::Token.new(Netherite::TokenType::NUMBER, new_value))
       end
     end
   end
